34f73946452fed97fa44012855ceabf12fdb0946
danilomakstds <100199794+danilomakstds@users.noreply.github.com>
2022-06-28 05:23:08 +0800

    Added process.env.PORT

--- main.js
+++ main.js
@@ -8,7 +8,11 @@ http.createServer(function (request, response) {
     
     // Send the response body as "Hello World"
     response.end('Hello World\n');
- }).listen(8081);
+ }).listen(3000);
+
+ app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
  
  // Console will print the message
- console.log('Server running at http://127.0.0.1:8081/');
+ console.log('Server running at http://127.0.0.1:3000/');
